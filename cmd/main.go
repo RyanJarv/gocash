89698c7bba3cf38a12a45f00e19e16e752209567
Ryan Gerstenkorn <ryan_gerstenkorn@fastmail.fm>
2019-09-07 02:57:49 -0700

    Debug flag isn't used for now

--- cmd/main.go
+++ cmd/main.go
@@ -9,11 +9,12 @@ import (
 func main() {
 	var ip = flag.String("ip", "127.0.0.1", "IP addr to bind and accept connections on")
 	var port = flag.Int("port", 6379, "Port number to listen on")
-	var debug = flag.Bool("debug", true, "Run in foreground with debug output enabled")
-	_, _, _ = port, ip, debug
+	//var debug = flag.Bool("debug", true, "Run in foreground with debug output enabled")
 
-	svr := gocash.NewServer(*ip, *port)
-	cache := gocash.NewCache()
-	process := gocash.NewProcess(svr, cache)
-	process.Start()
+	proc := gocash.NewProcess(
+		gocash.NewServer(*ip, *port),
+		gocash.NewCache(),
+	)
+
+	proc.Start()
 }
